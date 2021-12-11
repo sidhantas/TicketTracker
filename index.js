9de603910b4a5646f0a6aa8aac720afb099cebbe
Sidhanta Sharma <ssharm73@myucsc.edu>
2021-12-11 13:37:42 -0800

    counted total tickets remaining

--- index.js
+++ index.js
@@ -17,22 +17,25 @@ axios({
 }).then((response) => {
   const priceMap = parseText(process.argv[2]);
   let message = "";
+  let total_tickets = 0;
   for (let value of Object.values(response.data.listings)) {
     let section = parseInt(value.s);
     let price = value.sgp;
     let num_tickets = value.q;
-    
     let lookup = priceMap.get(section);
+    
     if (lookup && price <= lookup.price_limit && num_tickets >= lookup.num_tickets) {
       message += `Found Section: ${value.s}\n${value.rf}\nPrice: ${value.sgp}\n# of Tickets: ${value.q}\n`;
     }
+    total_tickets += value.q;
   }
+  console.log(total_tickets);
   if (message) {
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: process.env.RECEIVER_EMAIL,
       subject: "Twice Tickets",
-      text: message
+      text: `${message} \n Total Tickets Remaining: ${total_tickets}`
     };
     transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
